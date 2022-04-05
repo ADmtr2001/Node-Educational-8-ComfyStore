40565d11c94de4276a430858809906d22c11d30c
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-04-05 18:48:17 +0300

    add checkPermissions logic to getSingleUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,7 @@ const {
 	updateUserPassword
 } = require('../controllers/userController');
 
-router.route('/').get(authenticateUser, authorizePermissions('admin', 'user'), getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
 router.route('/showMe').get(authenticateUser, showCurrentUser);
 router.route('/updateUser').patch(authenticateUser, updateUser);
 router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
