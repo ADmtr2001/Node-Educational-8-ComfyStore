3cbe53c469dde46706566906b4f100042065567e
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2022-04-05 13:48:36 +0300

    add updateUserPassword logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,10 +10,10 @@ const {
 	updateUserPassword
 } = require('../controllers/userController');
 
-router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
+router.route('/').get(authenticateUser, authorizePermissions('admin', 'user'), getAllUsers);
 router.route('/showMe').get(authenticateUser, showCurrentUser);
 router.route('/updateUser').patch(updateUser);
-router.route('/updateUserPassword').patch(updateUserPassword);
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword);
 router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;
